24de3aafd2fab3f92aa891899bc540579e1bc4b6
shireenrao <shireenrao@gmail.com>
2013-07-25 08:12:49 -0400

    Initial bottle framework app

---
+++ todo/todo.py
@@ -0,0 +1,14 @@
+import sqlite3
+from bottle import route, run
+
+@route('/')
+@route('/todo')
+def todo_list():
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+    c.execute("SELECT id, task FROM todo WHERE status LIKE '1'")
+    result = c.fetchall()
+    return str(result)
+
+run()
+
