5e0369688b7ade3fdee031cfd7a3723b41e185a9
shireenrao <shireenrao@gmail.com>
2013-07-25 08:33:16 -0400

    Add template to display results

--- todo/todo.py
+++ todo/todo.py
@@ -1,5 +1,5 @@
 import sqlite3
-from bottle import route, run
+from bottle import route, run, debug, template, request
 
 @route('/')
 @route('/todo')
@@ -8,7 +8,24 @@ def todo_list():
     c = conn.cursor()
     c.execute("SELECT id, task FROM todo WHERE status LIKE '1'")
     result = c.fetchall()
-    return str(result)
+    c.close()
+    output = template('make_table', rows=result)
+    return output
 
-run()
+@route('/new', method='GET')
+def new_item():
+
+    new = request.GET.get('task', '').strip()
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO todo (task,status) VALUES (?,?)", (new,1))
+    new_id = c.lastrowid
+
+    conn.commit()
+    c.close()
+
+    return '<p>The task was inserted into the database,the ID is %s</p>' % new_id
+
+debug(True)
+run(reloader=True)
 
