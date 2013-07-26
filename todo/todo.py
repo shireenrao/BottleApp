9cb333e42846527cf7a4029e45ae5757b9b9eaba
shireenrao <shireenrao@gmail.com>
2013-07-25 22:25:58 -0400

    Add new_item method

--- todo/todo.py
+++ todo/todo.py
@@ -15,16 +15,19 @@ def todo_list():
 @route('/new', method='GET')
 def new_item():
 
-    new = request.GET.get('task', '').strip()
-    conn = sqlite3.connect('todo.db')
-    c = conn.cursor()
-    c.execute("INSERT INTO todo (task,status) VALUES (?,?)", (new,1))
-    new_id = c.lastrowid
+    if request.GET.get('save','').strip():
+        new = request.GET.get('task', '').strip()
+        conn = sqlite3.connect('todo.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO todo (task,status) VALUES (?,?)", (new,1))
+        new_id = c.lastrowid
 
-    conn.commit()
-    c.close()
+        conn.commit()
+        c.close()
 
-    return '<p>The task was inserted into the database,the ID is %s</p>' % new_id
+        return '<p>The task was inserted into the database,the ID is %s</p>' % new_id
+    else:
+        return template('new_task.tpl')
 
 debug(True)
 run(reloader=True)
